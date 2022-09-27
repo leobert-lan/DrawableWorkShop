7293aef4c7b3ca83e742619e5daaff080328a43e
leobert <leobert.l@hotmail.com>
2022-09-27 18:53:22 +0800

    feat: foo

--- DrawableWorkShop/foo/src/main/java/com/leobert/foo/foo/MainActivity.kt
+++ DrawableWorkShop/foo/src/main/java/com/leobert/foo/foo/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.leobert.foo.foo
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.leobert.foo.R
 
 class MainActivity : AppCompatActivity() {
